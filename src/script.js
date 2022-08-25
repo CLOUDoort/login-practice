340dc94783efb9e3dd1c527d4ea142582f0b604d
JunSeok <93777385+CLOUDoort@users.noreply.github.com>
2022-08-25 12:28:40 +0900

    Delete script.js

--- src/script.js
+++
@@ -1,4 +0,0 @@
-const email = document.getElementById('email')
-const emailInput = document.getElementById('emailInput')
-const password = document.getElementById('password')
-const passwordInput = document.getElementById('passwordInput')
