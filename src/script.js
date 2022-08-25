65ad63d064113c68a465da6f07cfd458e6519f28
JunSeok <ppu1234@naver.com>
2022-08-25 12:27:25 +0900

    first commit

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const email = document.getElementById('email')
+const emailInput = document.getElementById('emailInput')
+const password = document.getElementById('password')
+const passwordInput = document.getElementById('passwordInput')
